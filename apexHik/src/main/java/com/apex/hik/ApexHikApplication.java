2e99b4e0725cbb699395e3d9673da40aa64aee81
Md. Reazul Haque <37473674+reazulhaque20@users.noreply.github.com>
2022-09-11 17:00:17 +0600

    Initial Commit

---
+++ apexHik/src/main/java/com/apex/hik/ApexHikApplication.java
@@ -0,0 +1,13 @@
+package com.apex.hik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApexHikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApexHikApplication.class, args);
+	}
+
+}
---
+++ apexHik/src/test/java/com/apex/hik/ApexHikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apex.hik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApexHikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
